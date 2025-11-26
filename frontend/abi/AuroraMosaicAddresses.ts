3e501d4158f037166ab20a6163c4f4d9e3403836
BoydSweet <gunnarelijah913@gmail.com>
2025-11-26 06:15:53 +0000

    sepolia address

---
+++ frontend/abi/AuroraMosaicAddresses.ts
@@ -0,0 +1,12 @@
+export const AuroraMosaicAddresses = {
+  "31337": {
+    "address": "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+    "chainName": "localhost",
+    "chainId": 31337
+  },
+  "11155111": {
+    "address": "0x77E8332BBe91ef114dE256cBc6775d0D00d600a1",
+    "chainName": "sepolia",
+    "chainId": 11155111
+  }
+} as const;
